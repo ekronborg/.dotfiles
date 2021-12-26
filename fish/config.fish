427023459630889299e99c42640e9b19bfeac031
Emil Kronborg <emil.kronborg@protonmail.com>
2021-12-26 23:19:16 +0100

    added update alias and removed unused stuff

--- fish/config.fish
+++ fish/config.fish
@@ -5,25 +5,21 @@ bash "$HOME/.vim/plugged/gruvbox/gruvbox_256palette.sh"
 # https://stackoverflow.com/questions/866989/fish-interactive-shell-full-path
 function fish_prompt
         set_color normal
-        # if test $VIRTUAL_ENV
-        #     printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-        # end
         printf "["
         printf "%s" "$USER"
         printf "@"
         printf "%s" "$hostname"
         set_color "#8ec07c" --bold
         printf " %s" (prompt_long_pwd)
-        # printf " %s" (prompt_pwd)
         set_color normal
-        # printf "]\$ "
         printf "%s" (fish_git_prompt)
         printf "]\$ "
         set_color normal
 end
 
-# Using exa for more colors
+# Alias exa to ls for more colors
 alias ls='exa -al --color=always --group-directories-first'
+alias update='sudo apt update && sudo apt upgrade'
 
 # Set vim as default editor
 set -gx EDITOR vim
