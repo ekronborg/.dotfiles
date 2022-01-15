006fdb41e73979cfdcd16b679e92e69354d2a625
Emil Kronborg <emil.kronborg@protonmail.com>
2022-01-15 18:57:49 +0100

    changed update alias to up

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,3 +1,3 @@
-function update --description='alias for update and upgrade on Debian-based distros'
+function up --description='alias for update and upgrade on Debian-based distros'
     command sudo apt-get update && sudo apt-get upgrade -y
 end
