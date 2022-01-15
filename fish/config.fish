b5f774fa17bf3cc32930896bd29c0cbc3867ac04
Emil Kronborg <emil.kronborg@protonmail.com>
2022-01-15 20:48:49 +0100

    added support for colored man pages

--- fish/config.fish
+++ fish/config.fish
@@ -15,3 +15,12 @@ end
 
 # Set vim as default editor
 set -gx EDITOR vim
+
+# Add color to man pages
+set -xU LESS_TERMCAP_mb (printf "\e[01;35m")
+set -xU LESS_TERMCAP_md (printf "\e[01;36m")
+set -xU LESS_TERMCAP_me (printf "\e[0m")
+set -xU LESS_TERMCAP_se (printf "\e[0m")
+set -xU LESS_TERMCAP_so (printf "\e[01;34m")
+set -xU LESS_TERMCAP_ue (printf "\e[0m")
+set -xU LESS_TERMCAP_us (printf "\e[04m")
