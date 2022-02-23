c1783b3da22cefc863d2d88633390961460c301d
name <email>
2022-02-23 22:00:16 +0100

    Update and refactoring
    Remove directories if they exist and create symbolic links
    Add git, wget, and zsh directories
    Add .zshenv file
    Renamed .vimrc to vimrc since it now lives in $HOME/.vim
    Removed git-prompt.sh since it is now handled with a function instead

--- install.sh
+++ install.sh
@@ -9,29 +9,37 @@ cd $DIR
 # Dotfiles located in $HOME
 DOTFILES=(
     ".bashrc"
-    ".vimrc"
+    ".zshenv"
 )
 
-# Directories located in $HOME/.config/ (and git-prompt.sh)
+# Directories located in $HOME/.config/
 DOTDIRS=(
     "alacritty"
     "fish"
+    "git"
     "nvim"
     "tmux"
-    "git-prompt.sh"
+    "wget"
+    "zsh"
 )
+
+# Create directories if they do not exist already
+[[ ! -d $HOME/.config ]] && mkdir $HOME/.config
+[[ ! -d $HOME/.local/share ]] && mkdir -p $HOME/.local/share
+[[ ! -d $HOME/.cache/zsh ]] && mkdir -p $HOME/.cache/zsh
+[[ ! -d $HOME/.local/bin ]] && mkdir -p $HOME/.local/bin
 
 # Create symbolic links to dotfiles in $HOME
 for dotfile in "${DOTFILES[@]}"; do
     ln -sfv `pwd`/$dotfile $HOME/$dotfile
 done
 
-# Delete $HOME/.config/nvim if it exists
-if [[ -d $HOME/.config/nvim ]]; then
-    rm -rf $HOME/.config/nvim
-fi
-
 # Create symbolic links to directories in $HOME/.config/
 for dotdir in "${DOTDIRS[@]}"; do
+    [[ -d $HOME/.config/$dotdir ]] && rm -rf $HOME/.config/$dotdir
     ln -sfv `pwd`/$dotdir $HOME/.config/
 done
+
+# Create symbolic link from vimrc to $HOME/.vim
+ln -sfv `pwd`/vimrc $HOME/.vim/
+
