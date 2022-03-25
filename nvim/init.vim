ab62637f88724db6a0609e927d2d5e5cb95de5d9
Emil Kronborg Andersen <emil.kronborg@protonmail.com>
2022-03-25 18:07:02 +0100

    nvim: add telescope-fzf plugin

--- nvim/init.vim
+++ nvim/init.vim
@@ -9,6 +9,7 @@ Plug 'tpope/vim-commentary'
 Plug 'neovim/nvim-lspconfig'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
 Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
--- nvim/lua/telescope-config.lua
+++ nvim/lua/telescope-config.lua
@@ -1,12 +1,12 @@
--- Map <esc> to close Telescope popup
-local actions = require('telescope.actions')
-require('telescope').setup{
-  defaults = {
-    mappings = {
-      i = {
-        ["<esc>"] = actions.close
-      },
-    },
-  }
-}
-
+-- -- Map <esc> to close Telescope popup
+-- local actions = require('telescope.actions')
+-- require('telescope').setup{
+--   defaults = {
+--     mappings = {
+--       i = {
+--         ["<esc>"] = actions.close
+--       },
+--     },
+--   }
+-- }
+require('telescope').load_extension('fzf')
