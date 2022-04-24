47e57a7ea3e82a7f8cb36f7cc678714320e248ff
Emil Kronborg Andersen <emil.kronborg@protonmail.com>
2022-04-24 18:20:22 +0200

    user-overrides.js: this does not belong here, but I need to store it somewhere

---
+++ user-overrides.js
@@ -0,0 +1,7 @@
+/*** MY OVERRIDES ***/
+user_pref("_user.js.parrot", "overrides section syntax error");
+
+user_pref("privacy.resistFingerprinting.letterboxing", false); // Remove white borders
+user_pref("keyword.enabled", true); // Enable searching in the location bar
+
+user_pref("_user.js.parrot", "overrides section successful");
