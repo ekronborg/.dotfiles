f56989a0134bf999ed3d5e0bc46149c5dcdb830d
Emil Kronborg Andersen <emil.kronborg@protonmail.com>
2022-04-25 20:45:24 +0200

    polybar: change setup to dual bars

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,8 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-# polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# Launch Polybar and write to log files
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar primary 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Polybar launched..."
