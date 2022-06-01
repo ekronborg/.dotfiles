1587aa4fc7cbbc23b2be7753ca90bfddd4a0e8c2
Emil Kronborg Andersen <emil.kronborg@protonmail.com>
2022-06-01 20:37:21 +0200

    nvim: lualine: fix theme

--- nvim/lua/lualine-config.lua
+++ nvim/lua/lualine-config.lua
@@ -1,6 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
+    -- theme = 'auto',
     theme = 'gruvbox',
     -- component_separators = { left = '', right = ''},
     -- section_separators = { left = '', right = ''},
