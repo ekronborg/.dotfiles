713b66ad507e1e39fced7b340ec2fad7bcbbc4ff
Emil Kronborg Andersen <emil.kronborg@protonmail.com>
2022-11-08 20:34:09 +0100

    nvim: lsp: add experimental ghost text option

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -79,6 +79,9 @@ cmp.setup {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
+  experimental = {
+    ghost_text = true,
+  },
   mapping = {
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
