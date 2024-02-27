5d843463b026a5d132d3043404b13f82434a4596
Emil Kronborg <emil.kronborg@protonmail.com>
2024-02-27 21:22:38 +0100

    nvim: lsp: update signs
    The previous icons started rendering weird.

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -63,7 +63,7 @@ vim.diagnostic.config({
 })
 
 -- Show icons in the gutter instead of letters
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
