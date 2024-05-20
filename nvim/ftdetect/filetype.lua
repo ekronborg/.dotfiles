ac60b4d3ee001d03c260243c21b804dd888877bd
Emil Kronborg <emil.kronborg@protonmail.com>
2024-05-20 23:05:00 +0200

    nvim: filetype: add patterns for Yocto
    - By default, *.conf files are not detected in a conf/ folder in the
      root of the project.
    
    - *.inc files must be detected as bitbake if within a meta-layer
      unless a documentation directory.
    
    - *.config files for genimage recipes should be detected as conf instead
      of config.

--- nvim/ftdetect/filetype.lua
+++ nvim/ftdetect/filetype.lua
@@ -1,8 +1,14 @@
 -- See :h vim.filetype.add() and /usr/share/nvim/runtime/lua/vim/filetype.lua
 
 vim.filetype.add({
-	extension = {
-		config = "conf", -- *.config files used for genimage should be detected as conf
-		service = "systemd", -- *.service files in Yocto repo is not detected as systemd
-	},
+    extension = {
+        config = "conf", -- *.config files used for genimage should be detected as conf
+        service = "systemd", -- *.service files in Yocto repo is not detected as systemd
+    },
+    pattern = { -- https://neovim.io/doc/user/luaref.html#lua-patterns
+        [".*/conf/.*%.conf"] = "bitbake",
+        [".*/meta%-.*/.*%.inc"] = "bitbake",
+        [".*/meta%-.*/documentation/.*%.inc"] = "rst",
+        [".*/recipes%-images/.*%.config"] = "conf",
+    },
 })
