1d836a596ae01b888a4ad13c7f1b71e3e019d234
Emil Kronborg <emil.kronborg@protonmail.com>
2024-06-03 22:18:26 +0200

    nvim: lsp: add mappings for goto diagnostics
    Commit ("feat(diagnostic): add default mappings for diagnostics
    (#16230)") added goto mappings with 'float = false'.

--- nvim/lua/me/plugins/lsp/lspconfig.lua
+++ nvim/lua/me/plugins/lsp/lspconfig.lua
@@ -9,6 +9,19 @@ return {
         "williamboman/mason.nvim",
     },
     config = function()
+        -- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
+        -- vim.keymap.set("n", "[d", function()
+        --     vim.diagnostic.goto_prev()
+        --     vim.cmd("normal! zz")
+        -- end)
+        --
+        -- vim.keymap.set("n", "]d", function()
+        --     vim.diagnostic.goto_next()
+        --     vim.cmd("normal! zz")
+        -- end)
         -- Use LspAttach autocommand to only map the following keys
         -- after the language server attaches to the current buffer
         vim.api.nvim_create_autocmd("LspAttach", {
