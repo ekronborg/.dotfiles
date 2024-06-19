46e990b9afc8245d2e64787cb07353d0cae717a6
Emil Kronborg <emil.kronborg@protonmail.com>
2024-06-19 21:01:26 +0200

    nvim: headlines: add headline highlighting

--- nvim/lua/me/plugins/headlines.lua
+++ nvim/lua/me/plugins/headlines.lua
@@ -6,9 +6,12 @@ return {
         require("headlines").setup({
             vim.api.nvim_set_hl(0, "CodeBlock", { bg = gruvbox.dark0_hard }),
             vim.api.nvim_set_hl(0, "Dash", { fg = gruvbox.bright_yellow }),
+            vim.api.nvim_set_hl(0, "Headline1", { bg = gruvbox.dark_aqua_hard, fg = gruvbox.neutral_aqua }),
+            -- vim.api.nvim_set_hl(0, "Headline1", { bg = gruvbox.dark2, fg = gruvbox.light0 }),
             markdown = {
                 -- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
                 bullets = {},
+                headline_highlights = { "Headline1" },
             },
         })
         vim.opt.conceallevel = 2
