8d10438568727f452ca03934bfa946cc6aa7aa98
Emil Kronborg <emil.kronborg@protonmail.com>
2024-09-10 20:19:31 +0200

    nvim: disable loading of scripting languages

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
 -- Globals
 vim.g.mapleader = " " -- necessary for some plugins
 vim.g.c_syntax_for_h = 1 -- see :h c.vim
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 require("me.config")
 require("me.lazy")
