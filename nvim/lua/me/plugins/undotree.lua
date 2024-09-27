085a86cf3293294957ddaa624bdb339d8b0b6b37
Emil Kronborg <emil.kronborg@protonmail.com>
2024-09-27 10:41:10 +0200

    nvim: undotree: add some options to fix annoyances
    - Make the diff split useful by moving it to the bottom and at full
      width, so you can actually see what has changed.
    
    - Focus the undotree window when calling :UndotreeToggle.
    
    - Increase the width of undotree window so you can read full help menu
      text.

--- nvim/lua/me/plugins/undotree.lua
+++ nvim/lua/me/plugins/undotree.lua
@@ -2,6 +2,14 @@ return {
     -- Requires undodir to be set. Defaults to $XDG_STATE_HOME/nvim/undo/ and is automatically created
     -- Note that undofile must be true for persistence
     "mbbill/undotree",
+    -- vim.g.foo needs to be set in init(). See  https://github.com/LazyVim/LazyVim/discussions/258
+    init = function()
+        -- https://github.com/mbbill/undotree/blob/master/plugin/undotree.vim#L27
+        vim.g.undotree_WindowLayout = 2
+        vim.g.undotree_SetFocusWhenToggle = 1
+        vim.g.undotree_SplitWidth = 45 -- Enough to show help menu
+    end,
+
     keys = {
         { "<leader>u", "<cmd>UndotreeToggle<cr>", mode = { "n", "v" } },
     },
