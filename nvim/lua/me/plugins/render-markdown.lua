49f468d94c67514a48c51213e0b12555b495d1bf
Emil Kronborg <emil.kronborg@protonmail.com>
2025-01-10 18:47:23 +0100

    add render-markdown plugin
    The author of the headlines plugin will continue to maintain it, but
    probably not add new features. Instead, he suggests using
    render-markdown [1], which also has more features, so give it a go.
    
    [1]: https://github.com/lukas-reineke/headlines.nvim/blob/master/README.md?plain=1#L12-L15

---
+++ nvim/lua/me/plugins/render-markdown.lua
@@ -0,0 +1,73 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = "markdown",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local gruvbox = require("gruvbox").palette
+        vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = gruvbox.dark0_hard })
+        -- The 'highlight_inline' attribute for the 'code' block defaults to 'RenderMarkdownCode'.
+        -- If setting 'fg = gruvbox_bright_green' for 'RenderMarkdownCode', everything inside a code block gets
+        -- highlighted bright_green. If using the default value for 'highlight_inline', inline code in headings
+        -- become black boxes. To fix this, define a new highlight group and assign it to 'highlight_inline'.
+        vim.api.nvim_set_hl(0, "_InlineCode", { fg = gruvbox.bright_green, bg = gruvbox.dark0_hard })
+        -- stylua: ignore start
+        vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_blue, bold = true })
+        vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_yellow, bold = true })
+        vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_aqua, bold = true })
+        vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_purple, bold = true })
+        vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_green, bold = true })
+        vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_red, bold = true })
+        -- stylua: ignore end
+
+        require("render-markdown").setup({
+            render_modes = { "n", "v", "V", "\22", "c", "t" },
+
+            anti_conceal = {
+                enabled = true,
+                -- https://github.com/MeanderingProgrammer/render-markdown.nvim/wiki#anti-conceal
+                ignore = {
+                    head_icon = true,
+                    head_background = true,
+                    code_language = false,
+                    code_background = true,
+                    dash = true,
+                    bullet = true,
+                    quote = true,
+                    link = true,
+                    sign = true,
+                },
+            },
+
+            heading = {
+                sign = false,
+                -- icons = {},
+                -- position = "inline",
+                -- border = true,
+            },
+
+            code = {
+                disable_background = {},
+                width = "block",
+                sign = false,
+                -- left_pad = 1,
+                right_pad = 1,
+                -- highlight_inline = "",
+                highlight_inline = "_InlineCode",
+            },
+
+            pipe_table = {
+                head = "RenderMarkdownTableRow",
+            },
+
+            -- Disable plugin for floating windows and popups
+            -- https://github.com/MeanderingProgrammer/render-markdown.nvim/commit/873bdee806e381864a55f692bcbfe23269c8dc9d
+            -- https://github.com/MeanderingProgrammer/render-markdown.nvim/discussions/285
+            -- overrides = {
+            --     buflisted = { [false] = { enabled = false } },
+            -- },
+        })
+    end,
+}
