7d72a59c5cadf137add6e880e0fb7cb741710e17
Emil Kronborg <emil.kronborg@protonmail.com>
2025-01-10 18:38:49 +0100

    nvim: headlines: add colors to headings

--- nvim/lua/me/plugins/headlines.lua
+++ nvim/lua/me/plugins/headlines.lua
@@ -6,12 +6,17 @@ return {
         require("headlines").setup({
             vim.api.nvim_set_hl(0, "CodeBlock", { bg = gruvbox.dark0_hard }),
             vim.api.nvim_set_hl(0, "Dash", { fg = gruvbox.bright_yellow }),
-            vim.api.nvim_set_hl(0, "Headline1", { bg = gruvbox.dark_aqua_hard, fg = gruvbox.neutral_aqua }),
-            -- vim.api.nvim_set_hl(0, "Headline1", { bg = gruvbox.dark2, fg = gruvbox.light0 }),
+            vim.api.nvim_set_hl(0, "Headline1", { bg = gruvbox.bright_blue, fg = gruvbox.dark0 }),
+            vim.api.nvim_set_hl(0, "Headline2", { bg = gruvbox.bright_yellow, fg = gruvbox.dark0 }),
+            vim.api.nvim_set_hl(0, "Headline3", { bg = gruvbox.bright_aqua, fg = gruvbox.dark0 }),
+            vim.api.nvim_set_hl(0, "Headline4", { bg = gruvbox.bright_purple, fg = gruvbox.dark0 }),
+            vim.api.nvim_set_hl(0, "Headline5", { bg = gruvbox.bright_green, fg = gruvbox.dark0 }),
+            vim.api.nvim_set_hl(0, "Headline6", { bg = gruvbox.bright_red, fg = gruvbox.dark0 }),
             markdown = {
                 -- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
                 bullets = {},
-                headline_highlights = { "Headline1" },
+                headline_highlights = { "Headline1", "Headline2", "Headline3", "Headline4", "Headline5", "Headline6" },
+                fat_headlines = false,
             },
         })
         vim.opt.conceallevel = 2
