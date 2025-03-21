95830f8954735ec2ba1316435a1d31413c4974f0
Emil Kronborg <emil.kronborg@protonmail.com>
2025-03-21 22:32:31 +0100

    nvim: treesitter: add performance hacks from LazyVim
    Also, set 'version = false' to use the latest version.

--- nvim/lua/me/plugins/treesitter.lua
+++ nvim/lua/me/plugins/treesitter.lua
@@ -1,10 +1,19 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+    version = false,
+    dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
     event = { "LazyFile", "VeryLazy" },
     build = ":TSUpdate",
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    init = function(plugin)
+        -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+        -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+        -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+        -- Luckily, the only things that those plugins need are the custom queries, which we make available
+        -- during startup.
+        require("lazy.core.loader").add_to_rtp(plugin)
+        require("nvim-treesitter.query_predicates")
+    end,
     opts = {
         -- See https://github.com/nvim-treesitter/nvim-treesitter/issues/236 for "comment"
         ensure_installed = {
