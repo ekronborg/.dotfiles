66955c40eec11778a9bf75c83bfdaa90da1bb443
Emil Kronborg <emil.kronborg@protonmail.com>
2025-03-27 19:54:54 +0100

    nvim: markdown: move gruvbox options to gruvbox

--- nvim/lua/me/plugins/gruvbox.lua
+++ nvim/lua/me/plugins/gruvbox.lua
@@ -52,6 +52,16 @@ return {
             -- using markdownCode, which links to GruvboxAqua by default. Linking markdownCode to
             -- GruvboxFg1 instead fixes this issue and it doesn't seem to introduce any new issues.
             markdownCode = { link = "GruvboxFg1" },
+
+            -- For the render-markdown plugin.
+            RenderMarkdownCode = { bg = "#1d2021" },
+            RenderMarkdownH1Bg = { fg = "#1d2021", bg = "#83a598", bold = true },
+            RenderMarkdownH2Bg = { fg = "#1d2021", bg = "#fabd2f", bold = true },
+            RenderMarkdownH3Bg = { fg = "#1d2021", bg = "#8ec07c", bold = true },
+            RenderMarkdownH4Bg = { fg = "#1d2021", bg = "#d3869b", bold = true },
+            RenderMarkdownH5Bg = { fg = "#1d2021", bg = "#b8bb26", bold = true },
+            RenderMarkdownH6Bg = { fg = "#1d2021", bg = "#fb4934", bold = true },
+            _InlineCode = { fg = "#b8bb26", bg = "#1d2021" },
         },
     },
     config = function(_, opts)
--- nvim/lua/me/plugins/render-markdown.lua
+++ nvim/lua/me/plugins/render-markdown.lua
@@ -6,22 +6,6 @@ return {
         "echasnovski/mini.icons",
     },
     config = function()
-        local gruvbox = require("gruvbox").palette
-        vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = gruvbox.dark0_hard })
-        -- The 'highlight_inline' attribute for the 'code' block defaults to 'RenderMarkdownCode'.
-        -- If setting 'fg = gruvbox_bright_green' for 'RenderMarkdownCode', everything inside a code block gets
-        -- highlighted bright_green. If using the default value for 'highlight_inline', inline code in headings
-        -- become black boxes. To fix this, define a new highlight group and assign it to 'highlight_inline'.
-        vim.api.nvim_set_hl(0, "_InlineCode", { fg = gruvbox.bright_green, bg = gruvbox.dark0_hard })
-        -- stylua: ignore start
-        vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_blue, bold = true })
-        vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_yellow, bold = true })
-        vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_aqua, bold = true })
-        vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_purple, bold = true })
-        vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_green, bold = true })
-        vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = gruvbox.dark0_hard, bg = gruvbox.bright_red, bold = true })
-        -- stylua: ignore end
-
         require("render-markdown").setup({
             render_modes = { "n", "v", "V", "\22", "c", "t" },
 
@@ -55,6 +39,11 @@ return {
                 -- left_pad = 1,
                 right_pad = 1,
                 -- highlight_inline = "",
+
+                -- The 'highlight_inline' attribute for the 'code' block defaults to 'RenderMarkdownCode'.
+                -- If setting 'fg = gruvbox_bright_green' for 'RenderMarkdownCode', everything inside a code block gets
+                -- highlighted bright_green. If using the default value for 'highlight_inline', inline code in headings
+                -- become black boxes. To fix this, define a new highlight group and assign it to 'highlight_inline'.
                 highlight_inline = "_InlineCode",
             },
 
