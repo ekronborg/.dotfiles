2a976f47b7026ef906859fd8ce03d05bf35ee0e3
Emil Kronborg <emil.kronborg@protonmail.com>
2025-03-29 19:56:27 +0100

    nvim: snacks: enable bigfile

--- nvim/lua/me/plugins/snacks.lua
+++ nvim/lua/me/plugins/snacks.lua
@@ -3,6 +3,7 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
+        bigfile = { enabled = true },
         image = { enabled = true },
         notifier = { enabled = true },
         picker = {
