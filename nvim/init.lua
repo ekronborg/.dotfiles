4a9c3b717412b23686a7d4285b030042280a07ec
Emil Kronborg <emil.kronborg@protonmail.com>
2025-05-09 10:47:05 +0200

    nvim: fix treesitter flickering
    This is a huge performance decrease, so make sure to revert this commit
    once it is part of a Neovim release.

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,5 +6,9 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
+-- TODO: drop this when it is fixed in Neovim
+-- https://github.com/neovim/neovim/issues/32660
+vim.g._ts_force_sync_parsing = true
+
 require("me.config")
 require("me.lazy")
