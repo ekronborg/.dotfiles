3923078afd3144d88ac9fec20ee9354411a4d535
Emil Kronborg <emil.kronborg@protonmail.com>
2025-06-20 21:11:01 +0200

    nvim: autocmd: restore cursor position when opening a buffer
    This is taken from LazyVim.

--- nvim/lua/me/config/autocmds.lua
+++ nvim/lua/me/config/autocmds.lua
@@ -16,6 +16,24 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
     end,
 })
 
+-- go to last loc when opening a buffer
+vim.api.nvim_create_augroup("last_loc", { clear = true })
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(event)
+        local exclude = { "gitcommit" }
+        local buf = event.buf
+        if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+            return
+        end
+        vim.b[buf].lazyvim_last_loc = true
+        local mark = vim.api.nvim_buf_get_mark(buf, '"')
+        local lcount = vim.api.nvim_buf_line_count(buf)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
 vim.api.nvim_create_augroup("GitlabYaml", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     group = "GitlabYaml",
